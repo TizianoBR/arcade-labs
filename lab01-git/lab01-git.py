083b1da528afc12a54b9a35643c01105f94599e4
TizianoBR <tizianobiondo@hotmail.com>
2024-02-15 20:29:36 +0100

    v2.0

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -1,65 +1,88 @@
+import math
+import random
 import arcade
 
-arcade.open_window(800, 600, "My drawing")
+
+def draw_image(x, y, size):
+    arcade.draw_lrtb_rectangle_filled(size*0+x, size*800+x, size*150+y, size*0+y, arcade.color.DAVY_GREY)
+    arcade.draw_triangle_filled(size*70+x, size*130+y, size*150+x, size*130+y, size*110+x, size*270+y, (80, 80, 80))
+    arcade.draw_triangle_filled(size*775+x, size*115+y, size*695+x, size*115+y, size*735+x, size*350+y, (80, 80, 80))
+
+    arcade.draw_lrtb_rectangle_filled(size*0+x, size*800+x, size*600+y, size*525+y, (100, 100, 100))
+    arcade.draw_triangle_filled(size*500+x, size*550+y, size*540+x, size*550+y, size*520+x, size*425+y, (95, 95, 95))
+    arcade.draw_triangle_filled(size*450+x, size*570+y, size*510+x, size*570+y, size*480+x, size*390+y, (95, 95, 95))
+    arcade.draw_triangle_filled(size*15+x, size*565+y, size*55+x, size*565+y, size*35+x, size*415+y, (95, 95, 95))
+
+    #color = arcade.color.GOLD
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    arcade.draw_point(size*100+x, size*570+y, color, size*15)
+    arcade.draw_point(size*130+x, size*575+y, color, size*15)
+    arcade.draw_point(size*122.5+x, size*550+y, color, size*20)
+    arcade.draw_point(size*92.5+x, size*550+y, color, size*10)
+    arcade.draw_point(size*150+x, size*565+y, color, size*10)
+
+    #color = arcade.color.DIAMOND
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    arcade.draw_point(size*650+x, size*75+y, color, size*20)
+    arcade.draw_point(size*685+x, size*60+y, color, size*25)
+    arcade.draw_point(size*640+x, size*40+y, color, size*30)
+    arcade.draw_point(size*677.5+x, size*30+y, color, size*20)
+
+    #color = arcade.color.EMERALD
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    arcade.draw_point(size*400+x, size*560+y, color, size*20)
+    arcade.draw_point(size*430+x, size*550+y, color, size*15)
+    arcade.draw_point(size*425+x, size*575+y, color, size*15)
+
+    #color = arcade.color.LAPIS_LAZULI
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    arcade.draw_point(size*745+x, size*555+y, color, size*15)
+    arcade.draw_point(size*705+x, size*575+y, color, size*15)
+    arcade.draw_point(size*715+x, size*550+y, color, size*20)
+    arcade.draw_point(size*740+x, size*580+y, color, size*25)
+    arcade.draw_point(size*740+x, size*535+y, color, size*10)
+
+    #color = arcade.color.DARK_PASTEL_PURPLE
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    arcade.draw_point(size*80+x, size*50+y, color, size*20)
+    arcade.draw_point(size*107.5+x, size*30+y, color, size*20)
+    arcade.draw_point(size*110+x, size*65+y, color, size*30)
+    arcade.draw_point(size*75+x, size*20+y, color, size*25)
+    arcade.draw_point(size*135+x, size*15+y, color, size*15)
+    arcade.draw_point(size*140+x, size*50+y, color, size*20)
+
+    for i in range(0, 770, 50):
+        arcade.draw_line(size*(i+20)+x, size*70+y, size*(i+20)+x, size*110+y, arcade.color.WOOD_BROWN, size*5)
+    arcade.draw_line(size*0+x, size*70+y, size*800+x, size*70+y, (100, 100, 100), size*7.5)
+    arcade.draw_line(size*0+x, size*110+y, size*800+x, size*110+y, (100, 100, 100), size*7.5)
+
+    arcade.draw_circle_filled(size*617.5+x, size*145+y, size*12, arcade.color.YELLOW)
+    arcade.draw_lrtb_rectangle_filled(size*450+x, size*600+x, size*180+y, size*100+y, arcade.color.BROWN_NOSE)
+    arcade.draw_triangle_filled(size*450+x, size*181+y, size*450+x, size*100+y, size*420+x, size*181+y, arcade.color.BROWN_NOSE)
+    arcade.draw_triangle_filled(size*600+x, size*181+y, size*600+x, size*100+y, size*630+x, size*181+y, arcade.color.BROWN_NOSE)
+    arcade.draw_lrtb_rectangle_filled(size*410+x, size*640+x, size*190+y, size*170+y, (110, 110, 110))
+    arcade.draw_circle_filled(size*475+x, size*100+y, size*30, (110, 110, 110))
+    arcade.draw_circle_outline(size*475+x, size*100+y, size*22.5, arcade.color.BROWN_NOSE, size*15)
+    arcade.draw_circle_filled(size*575+x, size*100+y, size*30, (110, 110, 110))
+    arcade.draw_circle_outline(size*575+x, size*100+y, size*22.5, arcade.color.BROWN_NOSE, size*15)
+
+    arcade.draw_lrtb_rectangle_filled(size*225+x, size*325+x, size*550+y, size*125+y, (75, 75, 75))
+
+    arcade.draw_triangle_filled(size*435+x, size*0+y, size*295+x, size*0+y, size*365+x, size*275+y, (70, 70, 70))
+    arcade.draw_triangle_filled(size*335+x, size*0+y, size*215+x, size*0+y, size*275+x, size*210+y, (70, 70, 70))
+
+
+arcade.open_window(1500, 700, "My drawing")
 arcade.set_background_color(arcade.color.BATTLESHIP_GREY)
 arcade.start_render()
 
-arcade.draw_lrtb_rectangle_filled(0, 800, 150, 0, arcade.color.DAVY_GREY)
-arcade.draw_triangle_filled(70, 130,150, 130, 110, 270, (80, 80, 80))
-arcade.draw_triangle_filled(775, 115,695, 115, 735, 350, (80, 80, 80))
+random.seed()
 
-arcade.draw_lrtb_rectangle_filled(0, 800, 600, 525, (100, 100, 100))
-arcade.draw_triangle_filled(500, 550, 540, 550, 520, 425, (95, 95, 95))
-arcade.draw_triangle_filled(450, 570, 510, 570, 480, 390, (95, 95, 95))
-arcade.draw_triangle_filled(15, 565, 55, 565, 35, 415, (95, 95, 95))
-
-arcade.draw_point(100, 570, arcade.color.GOLD, 15)
-arcade.draw_point(130, 575, arcade.color.GOLD, 15)
-arcade.draw_point(122.5, 550, arcade.color.GOLD, 20)
-arcade.draw_point(92.5, 550, arcade.color.GOLD, 10)
-arcade.draw_point(150, 565, arcade.color.GOLD, 10)
-
-arcade.draw_point(650, 75, arcade.color.DIAMOND, 20)
-arcade.draw_point(685, 60, arcade.color.DIAMOND, 25)
-arcade.draw_point(640, 40, arcade.color.DIAMOND, 30)
-arcade.draw_point(677.5, 30, arcade.color.DIAMOND, 20)
-
-arcade.draw_point(400, 560, arcade.color.EMERALD, 20)
-arcade.draw_point(430, 550, arcade.color.EMERALD, 15)
-arcade.draw_point(425, 575, arcade.color.EMERALD, 15)
-
-arcade.draw_point(745, 555, arcade.color.LAPIS_LAZULI, 15)
-arcade.draw_point(705, 575, arcade.color.LAPIS_LAZULI, 15)
-arcade.draw_point(715, 550, arcade.color.LAPIS_LAZULI, 20)
-arcade.draw_point(740, 580, arcade.color.LAPIS_LAZULI, 25)
-arcade.draw_point(740, 535, arcade.color.LAPIS_LAZULI, 10)
-
-arcade.draw_point(80, 50, arcade.color.DARK_PASTEL_PURPLE, 20)
-arcade.draw_point(107.5, 30, arcade.color.DARK_PASTEL_PURPLE, 20)
-arcade.draw_point(110, 65, arcade.color.DARK_PASTEL_PURPLE, 30)
-arcade.draw_point(75, 20, arcade.color.DARK_PASTEL_PURPLE, 25)
-arcade.draw_point(135, 15, arcade.color.DARK_PASTEL_PURPLE, 15)
-arcade.draw_point(140, 50, arcade.color.DARK_PASTEL_PURPLE, 20)
-
-for i in range(0, 770, 50):
-    arcade.draw_line(i+20, 70, i+20, 110, arcade.color.WOOD_BROWN, 5)
-arcade.draw_line(0, 70, 800, 70, (100, 100, 100), 7.5)
-arcade.draw_line(0, 110, 800, 110, (100, 100, 100), 7.5)
-
-arcade.draw_circle_filled(617.5, 145, 12, arcade.color.YELLOW)
-arcade.draw_lrtb_rectangle_filled(450, 600, 180, 100, arcade.color.BROWN_NOSE)
-arcade.draw_triangle_filled(450, 181, 450, 100, 420, 181, arcade.color.BROWN_NOSE)
-arcade.draw_triangle_filled(600, 181, 600, 100, 630, 181, arcade.color.BROWN_NOSE)
-arcade.draw_lrtb_rectangle_filled(410, 640, 190, 170, (110, 110, 110))
-arcade.draw_circle_filled(475, 100, 30, (110, 110, 110))
-arcade.draw_circle_outline(475, 100, 22.5, arcade.color.BROWN_NOSE, 15)
-arcade.draw_circle_filled(575, 100, 30, (110, 110, 110))
-arcade.draw_circle_outline(575, 100, 22.5, arcade.color.BROWN_NOSE, 15)
-
-arcade.draw_lrtb_rectangle_filled(225, 325, 550, 125, (75, 75, 75))
-
-arcade.draw_triangle_filled(435, 0,295, 0, 365, 275, (70, 70, 70))
-arcade.draw_triangle_filled(335, 0,215, 0, 275, 210, (70, 70, 70))
+#for i in range(3):
+#    r = random.random()*0.5 + 0.5
+#    draw_image(random.randint(0, 1500-round(800*r)), random.randint(0, 700-round(600*r)), r)
+draw_image(-100, 0, 1.166)
+draw_image(800*1.166-100, 0, 1.166)
 
 arcade.finish_render()
 arcade.run()
